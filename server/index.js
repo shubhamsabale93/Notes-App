7bb512899a4f706bbac5cb0442ee38579c65da8e
shubhamsabal_93 <shubhamsabale4733@gmail.com>
2024-04-22 13:01:31 +0530

    new ports

--- server/index.js
+++ server/index.js
@@ -112,7 +112,7 @@ app.delete("/notes/:id",async(req,res)=>{
     })
 
 })
-const PORT=5000;
+const PORT=process.env.PORT || 5000;
 
 app.listen(PORT,()=>{
     console.log(`Server is running on ${PORT} port`);
